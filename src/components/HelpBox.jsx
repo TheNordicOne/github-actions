d26f00a054b776a202ae1938b8500c458a6590b9
Alexander Pahn <alexander.pahn@outlook.com>
2025-01-20 12:54:50 +0100

    updated project

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,14 +1,19 @@
+import { string } from 'prop-types';
+
 import './HelpBox.css';
 
 function HelpBox({ title, text }) {
-  const someEnvironmentVariable = process.env.SOME_ENVIRONMENT_VARIABLE;
   return (
     <article className="help-box">
       <h2>{title}</h2>
       <p>{text}</p>
-      <p>This is some environment variable: {someEnvironmentVariable}</p>
     </article>
   );
 }
+
+HelpBox.propTypes = {
+  title: string,
+  text: string,
+};
 
 export default HelpBox;
