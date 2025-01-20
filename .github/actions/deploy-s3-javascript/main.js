933d6ff38debbf29cead89d4e5172ee0eb713d99
Alexander Pahn <alexander.pahn@outlook.com>
2025-01-20 16:18:47 +0100

    Revert "Node Modules must not be excluded, as GitHub workflow does not download them"
    This reverts commit ec0f966dc00180c03442ede614222eb74a833168.

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
-// Node Modules must not be excluded, as GitHub workflow does not download them
+
 function run() {
     core.notice('Starting spending all your money 💸');
 }
