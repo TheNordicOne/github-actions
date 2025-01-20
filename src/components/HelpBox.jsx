b0576b8a7ad34db63b90091fed00e77880528535
Alexander Pahn <alexander.pahn@outlook.com>
2025-01-20 12:03:07 +0100

    add environment variable display in HelpBox component

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,10 +1,12 @@
 import './HelpBox.css';
 
 function HelpBox({ title, text }) {
+  const someEnvironmentVariable = process.env.SOME_ENVIRONMENT_VARIABLE;
   return (
     <article className="help-box">
       <h2>{title}</h2>
       <p>{text}</p>
+      <p>This is some environment variable: {someEnvironmentVariable}</p>
     </article>
   );
 }
